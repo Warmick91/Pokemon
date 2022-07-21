e71b5b89db875f5a749a44b80aa5ac8ab469fd73
Warmick91 <mickiewicz.dbass@gmail.com>
2022-07-21 14:54:55 +0200

    before the lesson finished

--- Pokemon/src/pokemon/IndigoLeague.java
+++ Pokemon/src/pokemon/IndigoLeague.java
@@ -6,7 +6,12 @@ import java.util.HashMap;
 import javax.swing.JOptionPane;
 
 public class IndigoLeague {
-
+	
+	public static void chooseAction() {
+		JOptionPane.showInputDialog(
+				"Choose action:\r\n 1) Attack\r\n 2) Defend\r\n 3) Use potion\r\n 4) Throw a pokeball");
+	}
+	
 	public static void main(String[] args) {
 
 		Bulbasaur Petalboy = new Bulbasaur("Petalboy", 8, "poison", 60, 30, 10, 15, 90, 0.1);
@@ -14,8 +19,9 @@ public class IndigoLeague {
 		String name1 = JOptionPane.showInputDialog("Prof. Oak: What's your name? I'm old and forgetful these days.");
 		String name2 = JOptionPane.showInputDialog("Prof. Oak: Ah, awesome. Who's your rival?");
 		Trainer player1 = new Trainer(name1, 3, 6);
-		Trainer player2 = new Trainer(name2, 3, 6);
-
+		Trainer player2 = new Trainer(name2, 3, 6);		
+		
+		
 		// The list of available Pokemon
 		ArrayList<Pokemon> pokedex = new ArrayList<Pokemon>();
 		pokedex.add(Petalboy);
@@ -26,27 +32,41 @@ public class IndigoLeague {
 		int choiceIntPokemon = Integer.parseInt(choicePokemon);
 
 		// Team building
+		String pokeSpecies1 = "";
+		String pokeSpecies2 = "";
 		HashMap<String, Pokemon> teams = new HashMap<String, Pokemon>();
 		switch (choiceIntPokemon) {
 		case 1:
 			teams.put(name1, pokedex.get(0));
 			teams.put(name2, pokedex.get(1));
+			pokeSpecies1 = "Bulbasaur";
+			pokeSpecies2 = "Charmander";
 			break;
 		case 2:
 			teams.put(name1, pokedex.get(1));
 			teams.put(name2, pokedex.get(0));
+			pokeSpecies1 = "Charmander";
+			pokeSpecies2 = "Bulbasaur";
 			break;
 		}
 
+		// Gameplay
 		System.out.println("Welcome to the Indigo League!\r\nToday's competitors are " + player1.getName() + " and "
 				+ player2.getName() + "!\r\n");
 
 		System.out.println(player1.getName() + ": " + teams.get(name1).getName() + ", go!");
-		System.out.println(player2.getName() + ": " + "Go, " + teams.get(name2).getName() + "!\r\n");
+		System.out.println(player1.getName() + " chooses " + pokeSpecies1 + "!\r\n");
 
-		System.out.println(Petalboy.getName() + "'s HP: " + Petalboy.getHealthPoints() + "\t" + Emberlass.getName()
-				+ "'s HP: " + Emberlass.getHealthPoints());
+		System.out.println(player2.getName() + ": " + "Go, " + teams.get(name2).getName() + "!");
+		System.out.println(player2.getName() + " chooses " + pokeSpecies2 + "!\r\n");
 
+		System.out.println(teams.get(name1).getName() + "'s HP: " + teams.get(name1).getHealthPoints() + "\t"
+				+ teams.get(name2).getName() + "'s HP: " + teams.get(name2).getHealthPoints());
+		
+		//Action choice
+		chooseAction();
+		
+		
 	}
 
 }
--- Pokemon/src/pokemon/Trainer.java
+++ Pokemon/src/pokemon/Trainer.java
@@ -10,8 +10,8 @@ public class Trainer {
 		this.potions = potions;
 		this.pokeballs = pokeballs;
 	}
-	
-	//Getters and setters
+
+	// Getters and setters
 	public String getName() {
 		return name;
 	}
@@ -35,21 +35,39 @@ public class Trainer {
 	public void setPokeballs(int pokeballs) {
 		this.pokeballs = pokeballs;
 	}
-	
-	//Actions
+
+	// Actions
 	public void orderAttack(Pokemon x) {
-		
+
 	}
-	
+
 	public void orderDefense(Pokemon x) {
-		
+
 	}
-	
+
 	public void orderPotion(Pokemon x) {
-		
+
 	}
-	
-	public void throwPokeball(Pokemon y) {
-		
+
+	public void throwPokeball() {
+
+	}
+
+	public void chooseAction(int x, Pokemon pok) {
+		switch (x) {
+		case 1:
+			orderAttack(pok);
+			break;
+		case 2:
+			orderDefense(pok);
+			break;
+		case 3:
+			orderPotion(pok);
+			break;
+		case 4:
+			throwPokeball();
+		default:
+			System.out.println("Prof. Oak: You need to choose a valid action, fool!");
+		}
 	}
 }
